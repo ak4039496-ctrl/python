35948bb21a25d29e664f8977aa4743b3ff36c07a
AMIT--GUPTA <ak4039496@gmail.com>
2026-06-30 08:43:09 +0530

    logical

---
+++ Python/Besic code in ptyhon/operator code in python/Logical Operators.py
@@ -0,0 +1,12 @@
+# basic_logical_operators.py
+# Author: Amit Gupta
+# Description: Demonstration of Python logical operators
+
+# Values
+x = True
+y = False
+
+# Logical Operators
+print("x and y  →", x and y)   # AND
+print("x or y   →", x or y)    # OR
+print("not x    →", not x)     # NOT
