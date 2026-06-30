0d614fe2edd53d9761d64aca0b77b99cb3cb2d27
AMIT--GUPTA <ak4039496@gmail.com>
2026-06-30 08:40:54 +0530

    Comparison-Operators

---
+++ Python/Besic code in ptyhon/operator code in python/Comparison Operators.py
@@ -0,0 +1,15 @@
+# basic_comparison_operators.py
+# Author: Amit Gupta
+# Description: Basic demonstration of Python comparison operators
+
+# Values
+a = 10
+b = 3
+
+# Comparison Operators
+print("a == b  →", a == b)   # Equal
+print("a != b  →", a != b)   # Not equal
+print("a > b   →", a > b)    # Greater than
+print("a < b   →", a < b)    # Less than
+print("a >= b  →", a >= b)   # Greater or equal
+print("a <= b  →", a <= b)   # Less or equal
