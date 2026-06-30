f70d5b843844e5e2453ad4db4887ddf995c2a44b
AMIT--GUPTA <ak4039496@gmail.com>
2026-06-30 08:45:24 +0530

    Membership Operators

---
+++ Python/Besic code in ptyhon/operator code in python/Membership Operators.py
@@ -0,0 +1,11 @@
+# basic_membership_operators.py
+# Author: Amit Gupta
+# Description: Demonstration of Python membership operators
+
+nums = [1, 2, 3, 4]
+
+# Membership Operators
+print("2 in nums      ->", 2 in nums)       # True
+print("5 not in nums ->", 5 not in nums)   # True 
+print("'a' in 'amit'  ->", 'a' in 'amit')   # True 
+print("'a' in 'Amit' ->",'a' in 'Amit')    #False
