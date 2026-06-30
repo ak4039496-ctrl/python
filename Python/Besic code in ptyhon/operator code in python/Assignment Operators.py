9c77c0ae94aeaaf3ddc559e4b810d1391c8f221a
AMIT--GUPTA <ak4039496@gmail.com>
2026-06-30 08:37:54 +0530

    Assignment Operators

---
+++ Python/Besic code in ptyhon/operator code in python/Assignment Operators.py
@@ -0,0 +1,27 @@
+# basic_assignment_operators.py
+# Author: Amit Gupta
+# Description: Demonstration of Python assignment operators
+
+# Initial value
+num = 5
+print("Initial value:", num)
+
+# Addition assignment
+num += 2   # num = num + 2
+print("After num += 2:", num)
+
+# Subtraction assignment
+num -= 1   # num = num - 1
+print("After num -= 1:", num)
+
+# Multiplication assignment
+num *= 3   # num = num * 3
+print("After num *= 3:", num)
+
+# Division assignment
+num /= 2   # num = num / 2
+print("After num /= 2:", num)
+
+# Modulus assignment
+num %= 4   # num = num % 4
+print("After num %= 4:", num)
