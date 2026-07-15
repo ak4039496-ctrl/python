98e1c19f5733082fcfccaea2877b2eeca791ae1c
AMIT--GUPTA <ak4039496@gmail.com>
2026-07-15 12:13:43 +0530

    bitwise_operators

---
+++ PYTHON/operator/bitwise_operators.py
@@ -0,0 +1,24 @@
+# Author:-Amit Kumar
+# Date:- 2026-07-10
+
+# Declare variables
+a = 6       # binary: 110
+b = 3       # binary: 011
+
+# Bitwise AND (&)
+print("a & b :-", a & b)
+
+# Bitwise OR (|)
+print("a | b :-", a | b)
+
+# Bitwise XOR (^)
+print("a ^ b :-", a ^ b)
+
+# Bitwise NOT (~)
+print("~a :-", ~a)
+
+# Left shift (<<)
+print("a << 1 :-", a << 1)
+
+# Right shift (>>)
+print("a >> 1 :-", a >> 1)
