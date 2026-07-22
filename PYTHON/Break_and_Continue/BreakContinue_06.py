b0cca7bb260a72d412bf81a2f27b0c89cd6bada3
AMIT--GUPTA <ak4039496@gmail.com>
2026-07-22 06:30:52 +0530

    BreakContinue_06

---
+++ PYTHON/Break_and_Continue/BreakContinue_06.py
@@ -0,0 +1,9 @@
+# Author: Amit Gupta
+# Date: 15-07-2026
+# Program: Continue on Even Numbers
+# Description: Prints only odd numbers
+
+for i in range(1, 11):
+    if i % 2 == 0:
+        continue
+    print(i)
